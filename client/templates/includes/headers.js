a41f5ff40c12d78c6bc4c02ddca4702fe5c4884b
Thomas Truongchau <ttruongchau@gmail.com>
2015-07-19 14:03:01 -0700

    added comment for note-to-self

--- client/templates/includes/headers.js
+++ client/templates/includes/headers.js
@@ -3,7 +3,10 @@ Template.header.helpers({
     var args = Array.prototype.slice.call(arguments, 0);
     args.pop();
 
+    // if any routes matches curr path return true
     var active = _.some(args, function(name) {
+      // false && someString returns false
+      // true && someString returns someString
       return Router.current() && Router.current().route.getName() === name;
     });
 
