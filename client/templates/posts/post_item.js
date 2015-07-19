67cd9b8b71eab03feb098ed6a6715c618eba7660
Thomas Truongchau <ttruongchau@gmail.com>
2015-07-19 12:44:56 -0700

    grey out upvote link when (1) not logged in or (2) already voted

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -6,11 +6,19 @@ Template.postItem.helpers({
   },
   ownPost: function() {
     return this.userId === Meteor.userId();
+  },
+  upvotedClass: function() {
+    var userId = Meteor.userId();
+    if (userId && !_.include(this.upvoters, userId)) {
+      return 'btn-primary upvotable'
+    } else {
+      return 'disabled';
+    };
   }
 });
 
 Template.postItem.events({
-  'click .upvote': function(e) {
+  'click .upvotable': function(e) {
     e.preventDefault();
     Meteor.call('upvote', this._id);
   }
